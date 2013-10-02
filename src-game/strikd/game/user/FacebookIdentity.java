7a73d7995de28c106dc3f6b397202b55831ab74d
Nils Weldon <nilsweldon@gmail.com>
2013-10-02 14:59:13 +0200

    Added access to the entire Facebook API by means of the Spring Social Framework

--- src-game/strikd/game/user/FacebookIdentity.java
+++ src-game/strikd/game/user/FacebookIdentity.java
@@ -1,9 +1,17 @@
 package strikd.game.user;
 
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.impl.FacebookTemplate;
+
 public class FacebookIdentity
 {
 	public long userId;
 	public String token;
+	
+	public Facebook getAPI()
+	{
+		return new FacebookTemplate(this.token);
+	}
 	
 	@Override
 	public String toString()
