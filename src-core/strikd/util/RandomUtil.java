f287eaf3621a14350beadb9abc41faca12595d5e
Nils Weldon <nilsweldon@gmail.com>
2013-10-27 13:19:33 +0100

    Added useful strikd.util.RandomUtil

---
+++ src-core/strikd/util/RandomUtil.java
@@ -0,0 +1,61 @@
+package strikd.util;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomUtil
+{
+	private static final ThreadLocalRandom getRng()
+	{
+		return ThreadLocalRandom.current();
+	}
+	
+	public static boolean flipCoin()
+	{
+		return getBool(0.5);
+	}
+	
+	public static boolean getBool(double p)
+	{
+		return (getRng().nextDouble() < p);
+	}
+	
+	public static int pickInt(int min, int max)
+	{
+		return getRng().nextInt(min, max + 1);
+	}
+
+	public static <T> T pickOne(T[] elements)
+	{
+		int length = elements.length;
+		if(length > 0)
+		{
+			return elements[getRng().nextInt(length)];
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public static <T> T pickOne(List<T> elements)
+	{
+		int size = elements.size();
+		if(size > 0)
+		{
+			return elements.get(getRng().nextInt(size));
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public static <T> T pickOne(Collection<T> elements)
+	{
+		List<T> list = new ArrayList<T>(elements);
+		return pickOne(list);
+	}
+}
