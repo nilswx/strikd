15d7defc3b5b72c4500b28625e9f3836dccf5f1b
Nils Weldon <nilsweldon@gmail.com>
2013-10-28 20:18:17 +0100

    Added hasClaimedLike to outgoing FacebookStatusMessage

--- src-communication/strikd/communication/outgoing/FacebookStatusMessage.java
+++ src-communication/strikd/communication/outgoing/FacebookStatusMessage.java
@@ -5,10 +5,10 @@ import strikd.net.codec.OutgoingMessage;
 
 public class FacebookStatusMessage extends OutgoingMessage
 {
-	public FacebookStatusMessage(boolean isLinked, boolean isLikeReceived)
+	public FacebookStatusMessage(boolean isLinked, boolean hasClaimedLike)
 	{
 		super(Opcodes.Outgoing.FACEBOOK_STATUS);
 		super.writeBool(isLinked);
-		super.writeBool(isLikeReceived);
+		super.writeBool(hasClaimedLike);
 	}
 }
