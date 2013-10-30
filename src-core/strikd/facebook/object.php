d4236ae0b9ce8a41c4dbfbfce6ff2cc7eb5fba3e
Nils Weldon <nilsweldon@gmail.com>
2013-10-30 02:12:51 +0100

    Did some tweakings to the OpenGraph object.php generator

--- src-core/strikd/facebook/object.php
+++ src-core/strikd/facebook/object.php
@@ -1,24 +1,20 @@
 <?php
-function curPageURL() {
- $pageURL = 'http://';
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
- return $pageURL;
-}
+$appId = 675897809090251;
+$appNs = 'strik-fb';
+$objectType = $_REQUEST['type'];
+$objectData = $_REQUEST['data'];
+
+// TODO: deal with this stuff, format image links etc
 ?>
 
 <html>
-  <head prefix="og: http://ogp.me/ns# product: http://ogp.me/ns/product#">
-    <meta property="fb:app_id" content="<?php echo strip_tags($_REQUEST['fb:app_id']);?>">
-      <meta property="og:url" content="<?php echo strip_tags(curPageURL());?>">
-      <meta property="og:type" content="<?php echo strip_tags($_REQUEST['og:type']);?>">
+  <head prefix="og: http://ogp.me/ns#">
+    <meta property="fb:app_id" content="<?= $appId ?>">
+      <meta property="og:type" content="<?= $appNs ?>:<?= $objectType ?>">
       <meta property="og:title" content="<?php echo strip_tags($_REQUEST['og:title']);?>">
       <meta property="og:image" content="<?php echo strip_tags($_REQUEST['og:image']);?>">
       <meta property="og:description" content="<?php echo strip_tags($_REQUEST['og:description']);?>">
-      <title>Product Name</title>
+      <title><?= $objectType ?></title>
   </head>
     <body>
       <?php echo strip_tags($_REQUEST['body']);?>
