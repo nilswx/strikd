0868c16a3291fd97f1f5df7312e18182e357cc02
Nils Weldon <nilsweldon@gmail.com>
2013-11-13 22:29:50 +0100

    Added an inverted lookup array for Direction8 (delta -> Direction8 calculation)

--- src-game/strikd/game/board/Direction8.java
+++ src-game/strikd/game/board/Direction8.java
@@ -12,55 +12,56 @@ public enum Direction8
 	SouthWest(-1, -1),
 	West(-1, 0),
 	NorthWest(-1, +1);
-	
+
 	public final int x;
 	public final int y;
-	
+
 	private Direction8(int deltaX, int deltaY)
 	{
 		this.x = deltaX;
 		this.y = deltaY;
 	}
-	
+
 	private static final Direction8[] values = values();
-	
+
 	public static Direction8[] all()
 	{
 		return values;
 	}
-	
+
 	public final Direction8 invert()
 	{
 		return values[(this.ordinal() + (values.length / 2)) % values.length];
 	}
-	
+
 	public final Direction8 transform(Direction8 dir)
 	{
 		return values[(this.ordinal() + dir.ordinal()) % values.length];
 	}
-	
+
 	public final boolean isDiagonal()
 	{
 		return (this.ordinal() % 2 != 0);
 	}
-	
+
 	public static final Direction8 random()
 	{
 		Random rand = new Random();
 		return values[rand.nextInt(values.length)];
 	}
 
-    public static final Direction8 directionFromDelta(int deltaX, int deltaY)
-    {
-        Direction8[] directions = Direction8.all();
-        for(int i = 0; i < directions.length; i++)
-        {
-            Direction8 direction = directions[i];
-            if(direction.x == deltaX && direction.y == deltaY)
-            {
-                return direction;
-            }
-        }
-        return null;
-    }
+	private static final Direction8[][] INVERTED_LOOKUP = new Direction8[3][3];
+
+	static
+	{
+		for(Direction8 dir : all())
+		{
+			INVERTED_LOOKUP[dir.x + 1][dir.y + 1] = dir;
+		}
+	}
+
+	public static final Direction8 directionFromDelta(int deltaX, int deltaY)
+	{
+		return INVERTED_LOOKUP[deltaX + 1][deltaY + 1];
+	}
 }
