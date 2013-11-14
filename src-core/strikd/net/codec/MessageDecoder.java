ace3dede1d06d75a1d5231afabd1993760e8dbd2
Nils Weldon <nilsweldon@gmail.com>
2013-11-14 15:39:08 +0100

    Sanity checks added to MessageDecoder

--- src-core/strikd/net/codec/MessageDecoder.java
+++ src-core/strikd/net/codec/MessageDecoder.java
@@ -9,28 +9,34 @@ import strikd.communication.Opcodes;
 
 public class MessageDecoder extends ByteToMessageDecoder
 {
+	private static final int MAX_MESSAGE_SIZE = 512;
+	
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf buffer, List<Object> out) throws Exception
 	{
-		// Mark reader index
-		buffer.markReaderIndex();
-
-		// Read message length
-		short length = buffer.readShort();
-
-		// Valid length and all data arrived?
-		if(length > 0 && buffer.isReadable(length))
+		// Length header available? (2 bytes)
+		if(buffer.isReadable(2))
 		{
-			// Parse opcode
-			Opcodes.Incoming op = Opcodes.Incoming.valueOf(buffer.readByte());
-			
-			// Copy bytes to an IncomingMessage (heapbuffer) and export this as a decoded message
-			out.add(new IncomingMessage(op, buffer.readBytes(length - 1)));
-		}
-		else
-		{
-			// Try again when there's more data
-			buffer.resetReaderIndex();
+			// Mark reader index
+			buffer.markReaderIndex();
+	
+			// Read message length (2 bytes)
+			short length = buffer.readShort();
+	
+			// Valid length and all data arrived?
+			if(length > 0 && length <= MAX_MESSAGE_SIZE && buffer.isReadable(length))
+			{
+				// Parse opcode
+				Opcodes.Incoming op = Opcodes.Incoming.valueOf(buffer.readByte());
+				
+				// Copy bytes to an IncomingMessage (heapbuffer) and export this as a decoded message
+				out.add(new IncomingMessage(op, buffer.readBytes(length - 1)));
+			}
+			else
+			{
+				// Restart reading again when there's more data
+				buffer.resetReaderIndex();
+			}
 		}
 	}
 }
