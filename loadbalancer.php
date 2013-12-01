9c84ee78c86a914be6788b95168af23565331382
Nils Weldon <nilsweldon@gmail.com>
2013-12-01 16:36:17 +0100

    Added loadbalancer.php

---
+++ loadbalancer.php
@@ -0,0 +1,7 @@
+<?php
+// Connect to database (use the most simple functions, no PDO, and use a persistent connection pool)
+
+// SELECT host,port FROM servers ORDER BY online_players LIMIT 1
+
+// Echo host + port
+?>
