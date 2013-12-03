bb9be51f719849118fc12d5d4b6b480c4028e091
Nils Weldon <nilsweldon@gmail.com>
2013-12-03 15:30:45 +0100

    PlayerDefaults now features a random avatar generator! (with gender!)

--- src-game/strikd/game/player/PlayerDefaults.java
+++ src-game/strikd/game/player/PlayerDefaults.java
@@ -1,5 +1,6 @@
 package strikd.game.player;
 
+import strikd.game.items.AvatarPart;
 import strikd.game.items.ItemInventory;
 import strikd.game.items.ItemType;
 import strikd.util.RandomUtil;
@@ -35,53 +36,65 @@ public class PlayerDefaults
 	}
 	
 	public void giveDefaultAvatar(Player player)
-	{
-		// Default parts for everyone
-		final ItemType[] stockParts =
-		{
-			// Generic hair
-			_I("HR_BLOND_M"),
-			_I("HR_BLACK_M"),
-			_I("HR_BROWN_M"),
-			_I("HR_RED_M"),
-			_I("HR_BLOND_F"),
-			_I("HR_BLACK_F"),
-			_I("HR_BROWN_F"),
-			_I("HR_RED_F"),
-				
-			// Race-friendly heads!
-			_I("HD_WHITE_M"),
-			_I("HD_WHITE_F"),
-			_I("HD_BLACK_M"),
-			_I("HD_BLACK_F"),
-			
-			// Generic eyes
-			_I("EY_BLUE"),
-			_I("EY_BROWN"),
-			_I("EY_GREEN"),
-			
-			// Generic mouths
-			_I("MO_M"),
-			_I("MO_F"),
-			
-			// Generic bodies
-			_I("BD_M"),
-			_I("BD_F")
-		};
+	{	
+		final int MALE = 0, FEMALE = 1;
 		
-		// Add them to the inventory
-		for(ItemType part : stockParts)
+		// Add stock bodies
+		final ItemType[][] bodies =
 		{
-			player.getInventory().add(part);
-		}
+			{ _I("BD_M") },
+			{ _I("BD_F") },
+		};
+		for(ItemType part : bodies[MALE]) player.getInventory().add(part);
+		for(ItemType part : bodies[FEMALE]) player.getInventory().add(part);
+		
+		// Add stock heads
+		final ItemType[][] heads =
+		{
+			{ _I("HD_WHITE_M"), _I("HD_BLACK_M") },
+			{ _I("HD_WHITE_F"), _I("HD_BLACK_F") },
+		};
+		for(ItemType part : heads[MALE]) player.getInventory().add(part);
+		for(ItemType part : heads[FEMALE]) player.getInventory().add(part);
+		
+		// Add stock mouths
+		final ItemType[][] mouths =
+		{
+			{ _I("MO_M") },
+			{ _I("MO_F") },
+		};
+		for(ItemType part : mouths[MALE]) player.getInventory().add(part);
+		for(ItemType part : mouths[FEMALE]) player.getInventory().add(part);
+		
+		// Add stock eyes
+		final ItemType[] eyes =
+		{
+			_I("EY_BLUE"), _I("EY_BROWN"), _I("EY_GREEN"),
+		};
+		for(ItemType part : eyes) player.getInventory().add(part);
+		
+		// Add stock hair
+		final ItemType[][] hair =
+		{
+			{ _I("HR_BLOND_M"), _I("HR_BLACK_M"), _I("HR_BROWN_M"), _I("HR_RED_M") },
+			{ _I("HR_BLOND_F"), _I("HR_BLACK_F"), _I("HR_BROWN_F"), _I("HR_RED_F") },
+		};
+		for(ItemType part : hair[MALE]) player.getInventory().add(part);
+		for(ItemType part : hair[FEMALE]) player.getInventory().add(part);
+		
+		// Save inventory
 		player.saveInventory();
 		
+		// Pick a random gender
+		int gender = RandomUtil.flipCoin() ? 1 : 0;
+		
 		// Now generate a random avatar from the stock parts
-		player.setAvatar("");
-		/*Avatar avatar = player.getAvatar();
-		for(AvatarPart part : parts)
-		{
-			avatar.set(part);
-		}*/
+		Avatar ava = new Avatar();
+		ava.set((AvatarPart)RandomUtil.pickOne(bodies[gender]));
+		ava.set((AvatarPart)RandomUtil.pickOne(heads[gender]));
+		ava.set((AvatarPart)RandomUtil.pickOne(mouths[gender]));
+		ava.set((AvatarPart)RandomUtil.pickOne(eyes));
+		ava.set((AvatarPart)RandomUtil.pickOne(hair[gender]));
+		player.setAvatar(ava.toString());
 	}
 }
