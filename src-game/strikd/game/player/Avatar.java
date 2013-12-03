e63c9c34c73f277de8bcbe75c6ec848946229337
Nils Weldon <nilsweldon@gmail.com>
2013-12-03 13:29:42 +0100

    More getters & setters for working with Avatar

--- src-game/strikd/game/player/Avatar.java
+++ src-game/strikd/game/player/Avatar.java
@@ -3,27 +3,45 @@ package strikd.game.player;
 import strikd.game.items.AvatarPart;
 import strikd.game.items.AvatarPart.PartType;
 
-public class Avatar
+public final class Avatar
 {
 	private final AvatarPart[] parts = new AvatarPart[PartType.values().length];
 	
-	public AvatarPart getPart(PartType type)
+	public final AvatarPart get(PartType type)
 	{
 		return this.parts[type.ordinal()];
 	}
 	
-	public void setPart(AvatarPart part)
+	public final boolean has(PartType type)
 	{
-		this.parts[part.getType().ordinal()] = part;
+		return (this.get(type) != null);
 	}
 	
-	public boolean hasPart(PartType type)
+	public final boolean hasPart(AvatarPart part)
 	{
-		return (this.getPart(type) != null);
+		return (this.get(part.getType()) == part);
 	}
 	
-	public boolean hasPart(AvatarPart part)
+	private final void set(PartType type, AvatarPart part)
 	{
-		return (this.getPart(part.getType()) == part);
+		this.parts[type.ordinal()] = part;
+	}
+	
+	public final void set(AvatarPart part)
+	{
+		this.set(part.getType(), part);
+	}
+	
+	public final void remove(PartType type)
+	{
+		this.set(type, null);
+	}
+	
+	public final void remove(AvatarPart part)
+	{
+		if(this.hasPart(part))
+		{
+			this.remove(part.getType());
+		}
 	}
 }
