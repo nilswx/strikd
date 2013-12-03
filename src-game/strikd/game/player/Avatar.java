46f1d8a2c7701e71624b6a01df09d202883ea291
Nils Weldon <nilsweldon@gmail.com>
2013-12-03 13:36:09 +0100

    Added Avatar#toString()

--- src-game/strikd/game/player/Avatar.java
+++ src-game/strikd/game/player/Avatar.java
@@ -5,43 +5,64 @@ import strikd.game.items.AvatarPart.PartType;
 
 public final class Avatar
 {
+	private static final char PART_DELIMITER = ':';
+	
 	private final AvatarPart[] parts = new AvatarPart[PartType.values().length];
 	
-	public final AvatarPart get(PartType type)
+	public AvatarPart get(PartType type)
 	{
 		return this.parts[type.ordinal()];
 	}
 	
-	public final boolean has(PartType type)
+	public boolean has(PartType type)
 	{
 		return (this.get(type) != null);
 	}
 	
-	public final boolean hasPart(AvatarPart part)
+	public boolean hasPart(AvatarPart part)
 	{
 		return (this.get(part.getType()) == part);
 	}
 	
-	private final void set(PartType type, AvatarPart part)
+	private void set(PartType type, AvatarPart part)
 	{
 		this.parts[type.ordinal()] = part;
 	}
-	
-	public final void set(AvatarPart part)
+
+	public void set(AvatarPart part)
 	{
 		this.set(part.getType(), part);
 	}
 	
-	public final void remove(PartType type)
+	public void remove(PartType type)
 	{
 		this.set(type, null);
 	}
 	
-	public final void remove(AvatarPart part)
+	public void remove(AvatarPart part)
 	{
 		if(this.hasPart(part))
 		{
 			this.remove(part.getType());
 		}
+	}
+	
+	@Override
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		boolean sep = false;
+		for(int i = 0; i < this.parts.length; i++)
+		{
+			if(this.parts[i] != null)
+			{
+				if(sep) sb.append(PART_DELIMITER); else sep = true;
+				sb.append(this.parts[i].getType().code());
+				sb.append(this.parts[i].getId());
+			}
+		}
+		
+		return sb.toString();
 	}
 }
