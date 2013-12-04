905d24bc70dbc39a2962971b7c64404709c7204e
Nils Weldon <nilsweldon@gmail.com>
2013-12-04 14:28:55 +0100

    Added EXPERIENCE_ADDED

--- src-communication/strikd/communication/outgoing/ExperienceMessage.java
+++ src-communication/strikd/communication/outgoing/ExperienceMessage.java
@@ -7,7 +7,7 @@ public class ExperienceMessage extends OutgoingMessage
 {
 	public ExperienceMessage(int added, int newTotal)
 	{
-		super(Opcodes.Outgoing.EXPERIENCE);
+		super(Opcodes.Outgoing.EXPERIENCE_ADDED);
 		super.writeInt(added);
 		super.writeInt(newTotal);
 	}
