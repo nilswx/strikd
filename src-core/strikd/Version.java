ddcdd8317007a80638707aa2795843f2e22dfba5
Nils Weldon <nilsweldon@gmail.com>
2014-03-30 22:34:31 +0200

    Another test!

--- src-core/strikd/Version.java
+++ src-core/strikd/Version.java
@@ -3,7 +3,7 @@ package strikd;
 public class Version
 {
 	// TODO: return from .pom or something
-	private static final String version = "0.4.2-dev";
+	private static final String version = "0.4.3-dev";
 	
 	private Version() { }
 	
